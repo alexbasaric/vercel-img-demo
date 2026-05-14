a3345fd169640c71821705eb4039560f08fb54ed
alexbasaric <alexbasaric@gmailc.com>
2026-05-13 20:53:37 -0400

    minor update for testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,11 +58,10 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             </div>
             <Logo />
             <h1 className="mt-8 mb-4 text-base font-bold uppercase tracking-widest">
-              2022 Event Photos
+              Vercel Demo Image Gallery
             </h1>
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-              Our incredible Next.js community got together in San Francisco for
-              our first ever in-person conference!
+              Test
             </p>
             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
